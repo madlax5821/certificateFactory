4ed7fafdc8e85d43747b737bd6cd7dc0ef564f47
xiaofeifei <madlax5821@gmail.com>
2024-02-14 00:54:29 -0500

    first push from master

---
+++ src/main/java/com/xiaofei/certificatetest/service/impl/Feature2.java
@@ -0,0 +1,12 @@
+package com.xiaofei.certificatetest.service.impl;
+
+/**
+ * Author: xiaofei
+ * Date: 2024-02-14, 0:53
+ * Description:
+ */
+public class Feature2 {
+    public void function2FromDanishFromMain(){
+        System.out.println("function2FromDanishFromMain");
+    }
+}
