24f1399e8a6c325b307cc8327de00fcfa9ffd2cf
xiaofeifei <madlax5821@gmail.com>
2024-02-14 00:13:17 -0500

    function 3 developed by liam

--- src/main/java/com/xiaofei/certificatetest/service/impl/TestFeature1.java
+++ src/main/java/com/xiaofei/certificatetest/service/impl/TestFeature1.java
@@ -9,4 +9,8 @@ public class TestFeature1 {
     public void function1(){
         System.out.println("function 1 in feature 1 by Liam.");
     }
+
+    public void function3(){
+        System.out.println("function 3 in feature 1 by Liam.");
+    }
 }
