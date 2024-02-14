bc19effcc984793cc9345e22550673e6fb783a0c
Liam Li <65733347+madlax5821@users.noreply.github.com>
2024-02-14 00:12:47 -0500

    Function 2 by saif

--- src/main/java/com/xiaofei/certificatetest/service/impl/TestFeature1.java
+++ src/main/java/com/xiaofei/certificatetest/service/impl/TestFeature1.java
@@ -9,4 +9,8 @@ public class TestFeature1 {
     public void function1(){
         System.out.println("function 1 in feature 1 by Liam.");
     }
+
+    public void function2(){
+        System.out.println("function 2 in feature 1 by Saif.");
+    }
 }
