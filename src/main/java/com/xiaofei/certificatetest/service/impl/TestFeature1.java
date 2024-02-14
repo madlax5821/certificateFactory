c51e3169471edcdaafb451f195e0a400d4c8d088
xiaofeifei <madlax5821@gmail.com>
2024-02-14 00:09:13 -0500

    function 1 developed by liam

---
+++ src/main/java/com/xiaofei/certificatetest/service/impl/TestFeature1.java
@@ -0,0 +1,12 @@
+package com.xiaofei.certificatetest.service.impl;
+
+/**
+ * Author: xiaofei
+ * Date: 2024-02-14, 0:07
+ * Description:
+ */
+public class TestFeature1 {
+    public void function1(){
+        System.out.println("function 1 in feature 1 by Liam.");
+    }
+}
